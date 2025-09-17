80f19bbc3bf7527b8df2af5e6b791da57b37e27c
Sunmi-CS <sunmicasano@gmail.com>
2025-09-17 15:07:46 -0500

    eae

--- biblioteca_project/src/biblioteca/urls.py
+++ biblioteca_project/src/biblioteca/urls.py
@@ -1,5 +1,8 @@
+from django.test import TestCase
+
 from django.urls import path
 from . import views
+
 
 urlpatterns = [
     path('', views.lista_libros, name='lista_libros'),
--- biblioteca_project/src/biblioteca/views.py
+++ biblioteca_project/src/biblioteca/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Libros
 
-
 def lista_libros(request):
     libros = Libros.objects.all()
     return render(request, 'libros/libros_list.html', {'libros': libros})
